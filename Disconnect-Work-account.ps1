3d3cbf466f048acc02d3db30d7c99e3159d90e9d
Unname33A62 <ngquangthai93@live.com>
2024-09-13 14:52:00 +0700

    Add files via upload

---
+++ Disconnect-Work-account.ps1
@@ -0,0 +1,11 @@
+﻿# Stop Microsoft Teams process
+Stop-Process -Name ms-teams -Force -ErrorAction SilentlyContinue
+
+# Disconnect Work account
+Get-ItemProperty -Path "C:\Users\*\AppData\Local\Packages" | ForEach-Object { Remove-Item -Path "$_\Microsoft.AAD.BrokerPlugin*" -Recurse -Force | Out-Null }
+
+# Display warnings
+Write-Output "Windows Work account has disconnected. Please check Windows Work account Settings."
+
+# Pause execution for 15 seconds
+Start-Sleep -Seconds 15
